c1d3cec6b821b3cccb1c79a927b4d1f75e80b106
Dman54 <dr.d-man54@yandex.ru>
2019-06-30 15:21:40 +0700

    add login page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,18 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
+    '''
     link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
     page.open()                      # открываем страницу
 #     page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
     page.should_be_login_link()
+    '''
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
